6e2722b812ad43422e8d67dabb1d198fe912e2e8
Henry McGehee <hdmcgehee@gmail.com>
2020-06-22 12:54:35 -0400

    added jquery

--- js/app.js
+++ js/app.js
@@ -17,3 +17,12 @@ class pet {
     // morph
 }
 
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
