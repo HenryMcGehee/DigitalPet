d25dd1f656c992f23648209b6f9fd88281a3365b
Henry McGehee <hdmcgehee@gmail.com>
2020-06-22 14:47:45 -0400

    create html with button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const screen = document.querySelector('.screen');
+const startButton = document.querySelector('.start');
+const body = document.querySelector('body');
+
 class pet {
     constructor(){
         // hunger 1-10
@@ -16,4 +20,56 @@ class pet {
 
     // morph
 }
+const addPet = () => {
 
+    screen.removeChild(startButton);
+
+    const bottombar = document.createElement('div');
+    bottombar.setAttribute('class', 'bottomBar');
+    
+    const button1 = document.createElement('button');
+    button1.setAttribute('class', 'feed');
+    button1.innerText = 'Feed';
+    bottombar.appendChild(button1);
+
+    const button2 = document.createElement('button');
+    button2.setAttribute('class', 'lights');
+    button2.innerText = 'Light Toggle';
+    bottombar.appendChild(button2);
+
+    const button3 = document.createElement('button');
+    button3.setAttribute('class', 'play');
+    button3.innerText = 'Play';
+    bottombar.appendChild(button3);
+
+    
+    const buttons = document.querySelector('.interact');
+    console.log(buttons);
+
+    const name = document.createElement('h1');
+    name.setAttribute('id', 'name');
+    name.innerText = pet.name;
+
+    const pic = document.createElement('img');
+    //pic.setAttribute('class', 'img');
+    pic.src = "https://picsum.photos/400/600";
+
+    const newPet = document.createElement('SECTION');
+    newPet.setAttribute("class", "pet");
+
+    newPet.appendChild(name);
+    newPet.appendChild(pic);
+    newPet.appendChild(bottombar);
+    screen.appendChild(newPet);
+}
+
+const makePet = () => {
+
+    const newPetCLass = new pet(name);
+
+    addPet();
+}
+
+$(() => {
+    $('.start').on('click', makePet);
+});
