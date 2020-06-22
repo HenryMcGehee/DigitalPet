7f5e65a9a13f6170e900c73c5bb60a9b712fb34d
Henry McGehee <hdmcgehee@gmail.com>
2020-06-22 12:41:26 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+class pet {
+    constructor(){
+        // hunger 1-10
+        // sleepiness 1-10
+        // boredom 1-10
+        // age
+        // name
+    }
+
+    // function increase age
+    // function increase hunger
+    // function increase sleepiness
+    // function increase boredom
+
+    // function die
+
+    // morph
+}
+
