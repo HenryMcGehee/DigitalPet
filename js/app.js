0f18b8472053e30bfc0599a6df45d5c5003ac43d
Henry McGehee <hdmcgehee@gmail.com>
2020-06-22 17:47:55 -0400

    basic functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 const screen = document.querySelector('.screen');
 const startButton = document.querySelector('.start');
-const body = document.querySelector('body');
+
+let p = {};
+let petName = '';
+let started = false;
+let time = 0;
 
 class pet {
-    constructor(){
-        // hunger 1-10
-        // sleepiness 1-10
-        // boredom 1-10
-        // age
-        // name
+    constructor(name, age, hunger, sleepiness, boredom){
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
     }
 
     // function increase age
@@ -20,9 +24,41 @@ class pet {
 
     // morph
 }
+
+const namePet = () => {
+    petName = prompt('enter your pets name', 'pet');
+}
+
 const addPet = () => {
+    const newPetClass = new pet (petName, 0, 0, 0, 0);
+    p = newPetClass;
 
     screen.removeChild(startButton);
+
+    const sidebar = document.createElement('aside');
+    sidebar.setAttribute('class', 'info');
+
+    const info1 = document.createElement('h2');
+    info1.setAttribute('id', 'info1');
+    info1.innerText = `age: ${p.age}`;
+
+    const info2 = document.createElement('h2');
+    info2.setAttribute('id', 'info2');
+    info2.innerText = `hunger: ${p.hunger}`;
+
+    const info3 = document.createElement('h2');
+    info3.setAttribute('id', 'info3');
+    info3.innerText = `sleepiness: ${p.sleepiness}`;
+
+    const info4 = document.createElement('h2');
+    info4.setAttribute('id', 'info4');
+    info4.innerText = `boredom: ${p.boredom}`;
+
+    sidebar.appendChild(info1);
+    sidebar.appendChild(info2);
+    sidebar.appendChild(info3);
+    sidebar.appendChild(info4);
+
 
     const bottombar = document.createElement('div');
     bottombar.setAttribute('class', 'bottomBar');
@@ -48,10 +84,10 @@ const addPet = () => {
 
     const name = document.createElement('h1');
     name.setAttribute('id', 'name');
-    name.innerText = pet.name;
+    name.innerText = p.name;
 
     const pic = document.createElement('img');
-    //pic.setAttribute('class', 'img');
+    pic.setAttribute('class', 'displayPet');
     pic.src = "https://picsum.photos/400/600";
 
     const newPet = document.createElement('SECTION');
@@ -59,15 +95,60 @@ const addPet = () => {
 
     newPet.appendChild(name);
     newPet.appendChild(pic);
+    newPet.appendChild(sidebar);
     newPet.appendChild(bottombar);
     screen.appendChild(newPet);
+
+    startTimer();
+
+    $('.feed').on('click', feedPet);
+    $('.lights').on('click', sleepPet);
+    $('.play').on('click', playPet);
 }
 
 const makePet = () => {
-
-    const newPetCLass = new pet(name);
+    namePet();
 
     addPet();
+}
+
+function startTimer(){
+    if(started === false){
+        started = true;
+
+        const timer = setInterval(function () {
+            time++;
+            
+            if(time % 360 === 0){
+                let ageNum = document.querySelector('#info1');
+                ageNum.innerText = `age: ${p.age++}`;
+            }
+            if(time % 6 === 0){
+                let sleepyNum = document.querySelector('#info3');
+                sleepyNum.innerText = `sleepiness: ${p.sleepiness++}`;
+            }
+            if(time % 6 === 0){
+                let hungerNum = document.querySelector('#info2');
+                hungerNum.innerText = `hunger: ${p.hunger++}`;
+            }
+            if(time % 2 === 0){
+                let boredNum = document.querySelector('#info4');
+                boredNum.innerText = `boredom: ${p.boredom++}`;
+            }
+        }, 100);
+    }
+}
+
+const feedPet = () => {
+    p.hunger = '0';
+}
+
+const playPet = () => {
+    p.boredom = '0';
+}
+
+const sleepPet = () => {
+    p.sleepiness = '0';
 }
 
 $(() => {
